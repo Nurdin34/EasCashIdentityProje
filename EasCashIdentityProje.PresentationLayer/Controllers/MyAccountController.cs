73c0e8a6f9b79a12e62a0b902e812b9d30a54cc8
Nurdin34 <tezcanurdin38@icloud.com>
2023-08-07 11:26:49 +0300

    Proje 4. günü

--- EasCashIdentityProje.PresentationLayer/Controllers/MyAccountController.cs
+++ EasCashIdentityProje.PresentationLayer/Controllers/MyAccountController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasCashIdentityProje.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasCashIdentityProje.PresentationLayer.Controllers
 {
+    [Authorize]
     public class MyAccountController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public MyAccountController(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async IActionResult Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
             return View();
         }
     }
